c1f2a9914019c63a9b37e516e6387a342e0fef15
Tatyanac94 <156218366+Tatyanac94@users.noreply.github.com>
2024-03-06 04:02:40 +0000

    Added an event listener to the second button that displays content within the empty unordered list on the html page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.getElementById("bttn1").addEventListener("click",ToggleEvent)
-function ToggleEvent() {
+document.getElementById("bttn1").addEventListener("click",CaseEvent)
+function CaseEvent() {
   let x = document.getElementById("myH1");
   if (x.style.textTransform == "uppercase") {
     x.style.textTransform = "lowercase";
@@ -8,3 +8,14 @@ function ToggleEvent() {
   }
 }
 
+
+document.getElementById("bttn2").addEventListener("click",DisplayEvent)
+function DisplayEvent() {
+  let x = document.getElementById("myUL");
+  let terms = ["Version Control", "Branch", "Commit"]
+  for (let i = 0; i < terms.length; i++) {
+    let li = document.createElement("li");
+    li.textContent= terms[i];
+    x.appendChild(li);
+  } 
+}
