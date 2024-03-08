174caf79a84a643eb63a24576b2b5beb724d851b
Tatyanac94 <156218366+Tatyanac94@users.noreply.github.com>
2024-03-08 02:51:17 +0000

    created an array of reviews

---
+++ reviews.js
@@ -0,0 +1,25 @@
+/*Create an array called reviews, which contains review objects. Each object has these properties: reviewerName (string data type), reviewText (string data type), and stars (number data type, 0-5). The array should contain 3-4 review objects. 
+Create a function displayReviews that runs on page load. The function should display a Bootstrap card for each review. Each card should display the reviewer's name, the review text, and number of stars. The Bootstrap cards should all display within the <div> with id of "review-cards".
+Add-Commit-Push these changes to GitHub. */
+let reviews = [
+  {
+    "reviewerName": "Harry Potter",
+    "reviewText": "This product is amazing!",
+    "stars": 5
+  },
+  {
+    "reviewerName": "Hermione Granger",
+    "reviewText": "I love this product!",
+    "stars": 4
+  },
+  {
+    "reviewerName": "Ron Weasley",
+    "reviewText": "The product is okay.",
+    "stars": 3
+  },
+  {
+    "reviewerName": "Draco Malfoy",
+    "reviewText": "This product sucks.",
+    "stars": 2
+  } 
+]
